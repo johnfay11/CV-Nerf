4fbf35a345b02bec650793036350c0b36215b665
daniela-wiepert <58085556+daniela-wiepert@users.noreply.github.com>
2021-04-12 02:35:26 -0400

    added llff data loading

--- data_helpers.py
+++ data_helpers.py
@@ -98,3 +98,50 @@ def load_blender_data(basedir, half_res=False, testskip=1):
             imgs_half_res[i] = cv2.resize(img, (W, H), interpolation=cv2.INTER_AREA)
         imgs = imgs_half_res
     return imgs, poses, render_poses, [H, W, focal], i_split, [2.0, 6.0]
+
+# custom llff loader modified from NERF implementation: https://github.com/bmild/nerf/blob/master/load_llff.py
+# allows us to use synthetic data
+def load_llff(topdir):
+    #location of the file containing poses and bounds - file created using COLMAP and imgs2poses.py from https://github.com/Fyusion/LLFF
+    # format of this file:
+    # np array (N,17) - each row, contains all pose matrix data + 2 depth values
+    # N = number of images
+    fpose = os.path.join(topdir,'poses_bounds.npy') 
+    poses_bounds = np.load(fpose) #load in file   
+    
+    # get only poses 
+    # format of poses after reshaping + transposing:
+    # (3,5,N)
+    # 3x4 camera-to-world transform + columns with [height, width, focal length]
+    # transpose handles weird storing of rotation in c2w transform matrix
+    poses = poses_bounds[:,:-2] 
+    poses = poses.reshape([-1,3,5]).transpose([1,2,0]) 
+    
+    #get only bounds - depth values
+    bounds = poses_bounds[:,-2:]
+    bounds = bounds.transpose([1,0]) 
+
+    #create list of images to read (contains image paths)
+    imgdir = os.path.join(topdir,'images')
+    #for each file in imgdir, if the file ends with png,jpg(JPG), add to list
+    images = []
+    for file in os.listdir(imgdir):
+        #TODO: double check this is returning the last 3 files
+        if file[-3:] == 'png' or file[-3:] == 'jpg' or file[-3:] == 'JPG':
+            images.append(os.path.join(imgdir,file))
+    
+
+    # make  sure image size is consistent
+    # img_shape = imageio.imread(images[0]).shape
+    # poses[:2,4,:] = np.array(img_shape[:2]).reshape([2,1]) #TODO: see how this reshape is happening
+
+    images_read = []
+    for file in images:
+        if file[-3:] == 'png':
+            i = imageio.imread(file,ignoregamma=True) 
+        else:
+            i = imageio.imread(file)
+        images_read.append(i[...,:3]/255.) #normalize images
+        
+    images = np.stack(images_read,-1) #TODO: figure out what this does
+    return poses, bounds, images
